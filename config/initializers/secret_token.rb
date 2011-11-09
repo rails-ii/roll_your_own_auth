e856b7fea9679e28a7800c26ccce38e38e672f25
Andrzej Krzywda <anrzejkrzywda@gmail.com>
2011-11-09 15:54:28 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RollYourOwn::Application.config.secret_token = '1c492506bba7e66954cf5f5740573b2091b589320ee627fd5dfdfc2b62e5e0239727410d7ad461fd2bcd06b078cd3ac528c191033f65797471b2fbb830301985'
